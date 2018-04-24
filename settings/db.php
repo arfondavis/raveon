92699127177b483eb9e08c4eb71be6f0c2014737
Arfon <arfon@matrix10.net>
2018-04-24 15:12:37 +0100

    gitignore

--- settings/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-ccc$db = new mysqli("localhost", "root", "password", "podcasts");
-
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
-?>

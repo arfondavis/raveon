f540cb66b6fa34072b43c06262d639b6b3b2d8f7
Arfon <arfon@matrix10.net>
2018-04-24 15:10:18 +0100

    testing gitignore

--- settings/db.php
+++ settings/db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("localhost", "root", "password", "podcasts");
+ccc$db = new mysqli("localhost", "root", "password", "podcasts");
 
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');

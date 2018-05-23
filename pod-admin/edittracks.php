d4a6f58efbb25a17b724f8fb37d98b01fe580c50
Arfon <arfon@matrix10.net>
2018-05-23 16:40:12 +0100

    added notify.js so have a nice alert on save/

--- pod-admin/edittracks.php
+++ pod-admin/edittracks.php
@@ -125,9 +125,6 @@
       $i++;
       } 
       ?>
-      <div class="col-12 pt-3">
-        <button class="btn btn-outline-success btn-sm float-right" name="newtrack" type="submit"><i class="fa fa-plus"></i></button>
-      </div>
   </div>
 </main>
 <!-- Bootstrap core JavaScript
@@ -138,6 +135,7 @@
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://rawgit.com/notifyjs/notifyjs/master/dist/notify.js"></script>
 <script type="text/javascript">
   $(document).ready(function(){
     var delay = (function(){
@@ -155,10 +153,16 @@
           method: "POST",
           data: form.serialize(),
           success: function(data){
-            console.log(data);
+            $(form).notify(
+              "Updated",
+              {
+                position:"top",
+                className: 'success',
+              }
+            );
           }
         })
-      }, 1500);
+      }, 900);
     });
   });
 </script>
